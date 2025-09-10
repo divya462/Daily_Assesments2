edc92783cbe1a2a485c721e2370e7eb33560a2c7
Divya kotte <153913545+divya462@users.noreply.github.com>
2025-09-10 16:17:35 +0530

    Update and rename BMI calculation to BMI.java

--- BMI.java
+++ BMI.java
@@ -1,3 +1,6 @@
+/*
+4 . Write a Java program that asks the user to enter their weight in kilograms and height in meters, then calculates and prints their Body Mass Index (BMI). Use the formula BMI = weight / (height * height).
+*/
 import java.util.Scanner;
 class BMI {
   public static void main(String[] args) {
