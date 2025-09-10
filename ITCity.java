36fec6690c99884048cc3b87cd395827473103d9
Divya kotte <153913545+divya462@users.noreply.github.com>
2025-09-10 16:16:40 +0530

    Update and rename IT City check to ITCity.java

--- ITCity.java
+++ ITCity.java
@@ -1,3 +1,6 @@
+/*
+6. Write a Java Program to check whether entered City is IT City or Not. Consider Delhi, Mumbai, Kolkatta, Bangalore, Chennai , Hyderabad as IT cities.
+*/
 import java.util.*;
 class ITCity {
   public static void main(String[] args) {
